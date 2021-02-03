713b2c55ef74495cd895243bcdabe4b213ebdf60
bannholtzer jonathan <bannholtzer.j@gmail.com>
2021-02-03 13:35:35 +0100

    mise a jour

--- src/PageInscription/Inscription.js
+++ src/PageInscription/Inscription.js
@@ -1,23 +1,18 @@
-import Forms from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 function Inscription() {
-  <div class="jumbotron jumbotron-fluid">
-    <div class="container">
-      <h1 class="display-3">Fluid jumbo heading</h1>
-      <p class="lead">Jumbo helper text</p>
-      <hr class="my-2" />
-      <p>More info</p>
-      <p class="lead">
-        <a
-          class="btn btn-primary btn-lg"
-          href="Jumbo action link"
-          role="button"
-        >
-          Jumbo action name
-        </a>
-      </p>
-    </div>
-  </div>;
+  return (
+    <Form>
+      <Form.Group controlId="formGroupEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+      </Form.Group>
+      <Form.Group controlId="formGroupPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+    </Form>
+  );
 }
 
 export default Inscription;
