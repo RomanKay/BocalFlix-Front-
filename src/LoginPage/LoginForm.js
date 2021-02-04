abf0fe7debbc4bebaf79fbeb4304397040a4d7ca
unknown <roman.kmy@gmail.com>
2021-02-04 14:52:42 +0100

    loginFormv2

--- src/LoginPage/LoginForm.js
+++ src/LoginPage/LoginForm.js
@@ -1,41 +1,46 @@
 import "./LoginForm.css";
 import { Link } from "react-router-dom";
-import BackgroundLogin from "./BackgroundLogin";
+
 function FormLog() {
   return (
     <div className="content">
-      <form action="">
-        <h2 className="TitleForm">S'identifier</h2>
+      <div id="FlixLogo">
+        <img src="Images\bocalLogo.png" alt="" />
+      </div>
+      <div className="content-box">
+        <form action="">
+          <h2 className="TitleForm">S'identifier</h2>
 
-        <input
-          type="text"
-          className="inp"
-          name="mail"
-          id=""
-          placeholder="Entrez votre email"
-        />
-        <input
-          type="text"
-          name="Mot de passe"
-          placeholder="Entrez votre mot de passe"
-        />
-        <input type="submit" value="connexion" />
-        <span className="box">
-          <input className="LogInp" type="checkbox" id="save" />
-          <label htmlFor="save">Se souvenir de moi</label>
-        </span>
+          <input
+            type="text"
+            className="inp"
+            name="mail"
+            id=""
+            placeholder="Entrez votre email"
+          />
+          <input
+            type="text"
+            name="Mot de passe"
+            placeholder="Entrez votre mot de passe"
+          />
+          <input type="submit" value="connexion" />
+          <span className="box">
+            <input className="LogInp" type="checkbox" id="save" />
+            <label htmlFor="save">Se souvenir de moi</label>
+          </span>
 
-        <p className="Visite">
-          Première visite sur Bocalflix?{" "}
-          <Link to="/inscription">S'inscrire</Link>
-        </p>
-        <div id="Social">
-          <img src="Images/Social/facebook.png" alt="" />
-          <p>
-            <a href="#">Se connecter via Facebook</a>
+          <p className="Visite">
+            Première visite sur Bocalflix?{" "}
+            <Link to="/inscription">S'inscrire</Link>
           </p>
-        </div>
-      </form>
+          <div id="Social">
+            <img src="Images/Social/facebook.png" alt="" />
+            <p>
+              <a href="#">Se connecter via Facebook</a>
+            </p>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
