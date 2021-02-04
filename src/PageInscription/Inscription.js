96d2979a1e0bfe71558b6a3061e3a15a7b62fea4
bannholtzer jonathan <bannholtzer.j@gmail.com>
2021-02-04 09:38:32 +0100

    Version1

--- src/PageInscription/Inscription.js
+++ src/PageInscription/Inscription.js
@@ -1,25 +1,3 @@
-{
-  /*import Forms from "react-bootstrap/Button";
-
-
-  /*function Inscription() {
-  <div class="jumbotron jumbotron-fluid">
-    <div class="container">
-      <h1 class="display-3">Fluid jumbo heading</h1>
-      <p class="lead">Jumbo helper text</p>
-      <hr class="my-2" />
-      <p>More info</p>
-      <p class="lead">
-        <a
-          class="btn btn-primary btn-lg"
-          href="Jumbo action link"
-          role="button"
-        >
-          Jumbo action name
-        </a>
-      </p>
-    </div>
-  </div>;
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
@@ -150,5 +128,3 @@ function Inscription() {
 }
 
 export default Inscription;
-/*/
-}
