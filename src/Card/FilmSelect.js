d55831574d3900d10c2ed5813c0ca927d938a39f
unknown <roman.kmy@gmail.com>
2021-02-05 16:36:53 +0100

    BocalFlixV1

--- src/Card/FilmSelect.js
+++ src/Card/FilmSelect.js
@@ -2,17 +2,26 @@ import "./FilmSelect.css";
 import Row from "react-bootstrap/Row";
 import FilmCard from "./Card.js";
 import { movies } from "../movies.json";
+import { series } from "../movies.json";
 
 function FilmSelect() {
-  function renderFilmCards() {}
+  function renderFilmCards() {
+    return movies.map(function (movie) {
+      return <FilmCard img={movie.img} />;
+    });
+  }
+  function renderSerieCards() {
+    return series.map(function (serie) {
+      return <FilmCard img={serie.img} />;
+    });
+  }
   return (
     <div className="Content">
       <h2 className="Categorie">Film</h2>
-      <Row>
-        <FilmCard img="img1.jpg" />
-        <FilmCard img="img1.jpg" />
-        <FilmCard img="img1.jpg" />
-      </Row>
+
+      <Row>{renderFilmCards()}</Row>
+      <h2 className="Categorie">Serie</h2>
+      <Row>{renderSerieCards()}</Row>
     </div>
   );
 }
