1f0b9986b2c33ae3139d557bbcadb6f1ae530da7
bannholtzer jonathan <bannholtzer.j@gmail.com>
2021-02-05 12:06:06 +0100

    v5

--- src/Carousel/Carousel.js
+++ src/Carousel/Carousel.js
@@ -1,26 +1,21 @@
 // Import Css
 import "./carousel.css";
 import Carousel from "react-bootstrap/Carousel";
-import horror from "./Images/dracula.jpg";
-import Marvel from "./Images/marvel.jpg";
-import StarWars from "./Images/Star_Wars.svg";
+
 function Slider() {
   return (
     <Carousel>
-      <Carousel.Item id="horror">
-        <img className="d-block w-100" src={horror} alt="First slide" />
+      <Carousel.Item className="slider horror">
         <Carousel.Caption />
       </Carousel.Item>
 
-      <Carousel.Item id="marvel">
-        <img className="d-block h-100" src={Marvel} alt="Third slide" />
+      <Carousel.Item className="slider marvel">
         <Carousel.Caption>
           <p>Découvrez la licence Marvel.</p>
         </Carousel.Caption>
       </Carousel.Item>
 
-      <Carousel.Item id="starwars">
-        <img className="d-block w-100" src={StarWars} alt="Third slide" />
+      <Carousel.Item className="slider starwars">
         <Carousel.Caption />
       </Carousel.Item>
     </Carousel>
