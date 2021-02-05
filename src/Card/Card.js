5c106e5959a12a9bb91280caa87095fcc979c512
unknown <roman.kmy@gmail.com>
2021-02-05 13:54:04 +0100

    CardTest1

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,19 +1,95 @@
 import "./Card.css";
+import Row from "react-bootstrap/Row";
 import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+
 function FilmSelect() {
   return (
     <div className="Content">
-      <Card className="bg-dark text-white">
-        <Card.Img src="holder.js/100px270" alt="Card image" />
-        <Card.ImgOverlay>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
-          </Card.Text>
-          <Card.Text>Last updated 3 mins ago</Card.Text>
-        </Card.ImgOverlay>
-      </Card>
+      <h2 className="Film">Film</h2>
+      <Row>
+        <Col sm="6" md="4" lg="3" xl="3">
+          <Card className="bg-dark text-white">
+            <Card.Img
+              className="BackImg"
+              id="ImgCard"
+              src="Images/Card/img1.jpg"
+              alt="Card image"
+            />
+            <Card.ImgOverlay>
+              <Card.Img
+                className="LogoBocals"
+                id="ImgCard"
+                src="Images/bocalLogo.png"
+                alt="Card image"
+              />
+            </Card.ImgOverlay>
+          </Card>
+        </Col>
+        <Col sm="6" md="4" lg="3" xl="3">
+          <Card className="bg-dark text-white">
+            <Card.Img
+              className="BackImg"
+              id="ImgCard"
+              src="Images/Card/img1.jpg"
+              alt="Card image"
+            />
+            <Card.ImgOverlay>
+              <Card.Img
+                id="LogoBocal"
+                id="ImgCard"
+                src="Images/bocalLogo.png"
+                alt="Card image"
+              />
+
+              <Card.Text></Card.Text>
+              <Card.Text>Last updated 3 mins ago</Card.Text>
+            </Card.ImgOverlay>
+          </Card>
+        </Col>
+        <Col sm="6" md="4" lg="3" xl="3">
+          <Card className="bg-dark text-white">
+            <Card.Img
+              className="BackImg"
+              id="ImgCard"
+              src="Images/Card/img1.jpg"
+              alt="Card image"
+            />
+            <Card.ImgOverlay>
+              <Card.Img
+                id="LogoBocal"
+                id="ImgCard"
+                src="Images/bocalLogo.png"
+                alt="Card image"
+              />
+
+              <Card.Text></Card.Text>
+              <Card.Text>Last updated 3 mins ago</Card.Text>
+            </Card.ImgOverlay>
+          </Card>
+        </Col>
+        <Col sm="6" md="4" lg="3" xl="3">
+          <Card className="bg-dark text-white">
+            <Card.Img
+              className="BackImg"
+              id="ImgCard"
+              src="Images/Card/img1.jpg"
+              alt="Card image"
+            />
+            <Card.ImgOverlay>
+              <Card.Img
+                id="LogoBocal"
+                id="ImgCard"
+                src="Images/bocalLogo.png"
+                alt="Card image"
+              />
+
+              <Card.Text></Card.Text>
+              <Card.Text>Last updated 3 mins ago</Card.Text>
+            </Card.ImgOverlay>
+          </Card>
+        </Col>
+      </Row>
     </div>
   );
 }
