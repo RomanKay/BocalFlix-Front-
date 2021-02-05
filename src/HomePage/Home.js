6f34392df73858c6c2dbba02a836fba3fe2d6c65
unknown <roman.kmy@gmail.com>
2021-02-05 12:08:03 +0100

    CardTest1

--- src/HomePage/Home.js
+++ src/HomePage/Home.js
@@ -3,12 +3,14 @@ import "./home.css";
 
 import Nav from "../Nav&Logo/Nav.js";
 import Slider from "../Carousel/Carousel";
+import FilmSelect from "../Card/Card.js";
 
 function Home() {
   return (
     <div className="header">
       <Nav />
       <Slider />
+      <FilmSelect />
     </div>
   );
 }
